196f92e00e07587b8fa612fa32cd75f0416fb54f
Ivan Dolgov <shmuft@yandex.ru>
2026-07-22 22:08:38 +0700

    Добавлен проект для сборки bundle шрифтов под Stride 4.1

---
+++ RussianFont4_1/RussianFont4_1.Windows/RussianFont4_1App.cs
@@ -0,0 +1,15 @@
+using Stride.Engine;
+
+namespace RussianFont4_1
+{
+    class RussianFont4_1App
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
